815e3c9be6cbbfe8763065e89a23f67c5a216d76
jdypro <jdypro@gmail.com>
2022-03-29 05:31:35 +0900

    style.css adjust

--- main.go
+++ main.go
@@ -20,7 +20,15 @@ func main() {
 	if port == "" {
 		port = "3000"
 	}
-	mux := http.NewServeMux()
+	fs := http.FileServer(http.Dir("assets")) // 파일 서버 인스터스 시키기
+
+	mux := http.NewServeMux() // 새로운 라우터 등록
+
+	// /assets/style.css -->> /assets/를 날려버리고 style.css만 취한다.
+	// HandleFunc가 아닌 Handle을 사용함을 기억할것
+	// http.FileServer 메서드는 http.Handler 타입을 리턴한다.
+	mux.Handle("/assets/", http.StripPrefix("/assets", fs))
+
 	mux.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":"+port, mux)
 }
